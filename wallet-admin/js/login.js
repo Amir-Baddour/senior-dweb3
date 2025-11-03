a2b51ac3c36c49f78183b935108106a9076a68e3
Amir Baddour <amirbaddour675@gmail.com>
2025-11-03 22:25:36 +0200

    Fix: c in the login admin

--- wallet-admin/js/login.js
+++ wallet-admin/js/login.js
@@ -14,7 +14,7 @@ document.getElementById('adminLoginForm').addEventListener('submit', function(e)
                   localStorage.setItem('admin_jwt', response.data.token);
               }
               // Redirect to the admin dashboard
-              window.location.href = '/wallet-admin/dashboard.html';
+              window.location.href = 'dashboard.html';
           } else {
               // Log unexpected responses for debugging purposes
               console.error("Unexpected response from server.", response.data);
