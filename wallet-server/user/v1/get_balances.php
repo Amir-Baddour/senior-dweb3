6fe44086ea4615a5cae61aab9c2a0fe4904fed97
Amir Baddour <amirbaddour675@gmail.com>
2025-11-05 22:11:05 +0200

    Fix config.js script tag and load order

--- wallet-server/user/v1/get_balances.php
+++ wallet-server/user/v1/get_balances.php
@@ -1,7 +1,7 @@
 <?php
 // wallet-server/user/v1/get_balances.php
 ob_start();
-require_once __DIR__ . '/../../../utils/cors.php';
+require_once __DIR__ . '/../../utils/cors.php';
 require_once __DIR__ . '/../../../connection/db.php';
 require_once __DIR__ . '/../../../models/WalletsModel.php';
 require_once __DIR__ . '/../../../utils/verify_jwt.php';
