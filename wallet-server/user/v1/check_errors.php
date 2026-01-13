864613fed9a078e9f79a824b689ffa87c9406bc9
Amir Baddour <amirbaddour675@gmail.com>
2026-01-13 21:29:32 +0200

    cleaning the code

--- wallet-server/user/v1/check_errors.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Error log location: " . ini_get('error_log') . "\n";
-echo "Error reporting: " . error_reporting() . "\n";
-echo "Display errors: " . ini_get('display_errors') . "\n";
-?>
